ebea57e6c055ca210846161df0360f2b1367df09
Zhou Bey <931371256@qq.com>
2018-05-18 19:19:00 +0800

    修改桌面图标

--- app/src/main/java/yy/zpy/cc/memo/App.kt
+++ app/src/main/java/yy/zpy/cc/memo/App.kt
@@ -6,15 +6,18 @@ import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.StrictMode
+import android.text.TextUtils
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.tencent.bugly.crashreport.CrashReport
 import yy.zpy.cc.greendaolibrary.bean.DaoMaster
 import yy.zpy.cc.greendaolibrary.bean.FolderBeanDao
 import yy.zpy.cc.greendaolibrary.bean.ImageBeanDao
 import yy.zpy.cc.greendaolibrary.bean.MemoBeanDao
 import yy.zpy.cc.greendaolibrary.helper.GreenDaoOpenHelper
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.IOException
+import java.util.*
 import kotlin.properties.Delegates
 
 
@@ -47,6 +50,7 @@ class App : Application() {
         sp = getSharedPreferences("memo-data", MODE_PRIVATE)
         val builder = StrictMode.VmPolicy.Builder()
         StrictMode.setVmPolicy(builder.build())
+        initBugly()
     }
 
     private val isMainProcess: Boolean
@@ -87,6 +91,42 @@ class App : Application() {
         var instance: App by Delegates.notNull<App>()
             private set
     }
+
+    private fun initBugly() {
+        val context = applicationContext
+        // 获取当前包名
+        val packageName = context.packageName
+        // 获取当前进程名
+        val processName = getProcessName(android.os.Process.myPid())
+        // 设置是否为上报进程
+        val strategy = CrashReport.UserStrategy(context)
+        strategy.isUploadProcess = processName == null || processName == packageName
+        try {
+            strategy.appVersion = packageManager.getPackageInfo(packageName, 0).versionName
+        } catch (ignored: Exception) {
+        }
+
+        strategy.appPackageName = packageName  //App的包名
+        strategy.setCrashHandleCallback(object : CrashReport.CrashHandleCallback() {
+            override fun onCrashHandleStart(crashType: Int, errorType: String?,
+                                            errorMessage: String?, errorStack: String?): Map<String, String> {
+                val map = LinkedHashMap<String, String>()
+                map["Key"] = "Value"
+                return map
+            }
+
+            override fun onCrashHandleStart2GetExtraDatas(crashType: Int, errorType: String?,
+                                                          errorMessage: String?, errorStack: String?): ByteArray? {
+                return try {
+                    "Extra data.".toByteArray(charset("UTF-8"))
+                } catch (e: Exception) {
+                    null
+                }
+
+            }
+        })
+        CrashReport.initCrashReport(applicationContext, BuildConfig.BUGLY_APP_ID, BuildConfig.DEBUG, strategy)
+    }
 }
 
 fun logcat(log: Any?) {
@@ -101,4 +141,28 @@ fun generateTag(): String {
     tag = String.format(tag, callerClazzName, caller.methodName, caller.lineNumber)
     return tag
 }
+
 fun getScreenWidth(context: Context) = context.resources.displayMetrics.widthPixels
+fun getProcessName(pid: Int): String? {
+    var reader: BufferedReader? = null
+    try {
+        reader = BufferedReader(FileReader("/proc/$pid/cmdline"))
+        var processName = reader.readLine()
+        if (!TextUtils.isEmpty(processName)) {
+            processName = processName.trim { it <= ' ' }
+        }
+        return processName
+    } catch (throwable: Throwable) {
+        throwable.printStackTrace()
+    } finally {
+        try {
+            if (reader != null) {
+                reader.close()
+            }
+        } catch (exception: IOException) {
+            exception.printStackTrace()
+        }
+
+    }
+    return null
+}
